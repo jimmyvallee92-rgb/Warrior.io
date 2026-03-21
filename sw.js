cb869153cb2096b051dc1cdc86b163b68807c57e
jimmyvallee92-rgb <jimmyvallee92@gmail.com>
2026-03-21 18:35:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'warrior-io-v1';
+const FILES = [
+  '/Warrior.io/',
+  '/Warrior.io/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
